080209da6e032307761dead5496d08605f4dfe80
Jakub Štefček <jakub.stefcek@hotmail.cz>
2024-03-15 21:27:32 +0100

    Upravy v kodu pro presun pripadne smazani

--- util_move_vycet.py
+++ util_move_vycet.py
@@ -44,21 +44,32 @@ for file in vycetka_files:
             # Pokud je soubor starsiho data tak se presune a nebude uz na disku
             if m_timestamp > s_timestamp:
                 # Pokusi se prekopirovat soubor na Google DRIVE
+                print("COPY - " + file + str(m_timestamp))
                 shutil.copy(vycetka_path + "\\" + file, googledrive_path)
                 log2file(file, "INFO", "prekopirovan na Google Drive")
                 pocet_kopir = pocet_kopir + 1
 
             else:
                 # Pokusi se prekopirovat soubor na Google DRIVE
+                print("MOVE - " + file + str(m_timestamp))
                 shutil.move(vycetka_path + "\\" + file, googledrive_path)
                 log2file(file, "INFO", "presunut na Google Drive")
                 pocet_move = pocet_move + 1
+
+        # Pokud je jiz vycetka na Google Drive
+        except shutil.Error as ex:
+            log2file(file, "WARN", "jiz je na Google Drive a bude vymazan. Chyba: " + str(ex))
+            pocet_move = pocet_move + 1
+            os.remove(vycetka_path + "\\" + file)
 
         # Odchyceni chyby 
         except Exception as ex:
             log2file(file, "ERROR", "se nepodařilo presunout " + str(ex))
             raise Exception("ERROR - " + str(ex))
+        
+    else:
+        log2file("", "INFO", "neni k presunuti")
 
 # konec 
 file_time = datetime.datetime.today()
-print("[" + file_time.strftime("%Y-%m-%d %H:%M:%S:%f") +"]" + " - Celkem presunuto " + str(pocet_move) + " vycetek a prekopirovano " + str(pocet_kopir) + " vycetek")
+print("[" + file_time.strftime("%Y-%m-%d %H:%M:%S") +"]" + " [INFO] - Celkem presunuto: " + str(pocet_move) + " a prekopirovano: " + str(pocet_kopir) + " dennich vycetek")
